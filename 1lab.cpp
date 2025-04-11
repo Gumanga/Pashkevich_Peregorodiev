2c68b958197a9bcf6ecd47cb261f136e0d6036c4
pplaya <gleb19051904@gmail.com>
2025-04-11 10:10:46 +0300

    refactor: delete sorted from b

--- 1lab.cpp
+++ 1lab.cpp
@@ -8,10 +8,11 @@ class Decimate {
 public:
     string a, b;
 
-    void initialization() {
+    void init() {
         a = "";
         b = "";
     }
+
     void read() {
         cout << "Enter a: ";
         cin >> a;
@@ -20,44 +21,41 @@ public:
     }
 
     void arithmetic() {
+        
         int sortA = stoi(sortt(a));
         int sortB = stoi(sortt(b));
 
         cout << "sum: " << sortA + sortB << endl;
+        cout << "subtraction: " << sortA - sortB << endl;
         cout << "equals: " << (sortA == sortB ? "true" : "false") << endl;
         cout << "a > b: " << (sortA > sortB ? "true" : "false") << endl;
-        multiply(); // Call the new multiply function
     }
 
     void display() {
         cout << endl;
         
         cout << "Sorted a: " << sortt(a) << endl;
-        cout << "Sorted b: " << sortt(b) << endl;
         
         cout << endl;
     }
 
     string sortt(string& num) {
+
         bool isNegative = (num[0] == '-');
         string digits = (num[0] == '+' || num[0] == '-') ? num.substr(1) : num;
 
         sort(digits.rbegin(), digits.rend());
 
-        return isNegative ? "-" + digits : digits;
-    }
-
-    // New function to multiply the sorted numbers
-    void multiply() {
-        int sortA = stoi(sortt(a));
-        int sortB = stoi(sortt(b));
-        cout << "multiply: " << sortA * sortB << endl;
+        return
+        
+         isNegative ? "-" + digits : digits;
     }
 };
 
 int main() {
+
     Decimate obj;
-    obj.initialization();
+    obj.init();
     obj.read();
     obj.display();
     obj.arithmetic();
